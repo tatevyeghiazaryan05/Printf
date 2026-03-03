2a445281b12fdc029d9522a9ef1e53304e655a3c
Tatevik Yeghiazaryan <tyeghiaz@c2r14s3.42yerevan.am>
2026-03-03 23:01:51 +0400

    printf changing

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tyeghiaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 19:28:38 by tyeghiaz          #+#    #+#             */
-/*   Updated: 2026/02/25 21:10:47 by tyeghiaz         ###   ########.fr       */
+/*   Updated: 2026/03/03 23:00:32 by tyeghiaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,45 @@ int	ft_printf(const char *format, ... )
 {
 	va_list list;
 	int	count;
-
+	int	i;
 
 	count = 0;
+	i = 0;
 	va_start(list, format);
-	
-	va_arg();
-	va_end(list);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			int j;
+			if (format[i] == 'c')
+			{
+				j = va_arg(list, int);
+				count += ft_putchar(j);
+			}
+			else if (format[i] == 's')
+			{
+				char *s;
+
+				s = va_arg(list, char *);
+				count += ft_putstr(s);
+			}
+			else if (format[i] == 'p')
+			{
+				void *j;
+				
+				j = va_arg(list, void *);
+                                cont += ft_putstr(format[i]);
+			}
+			else if (format[i] == 'i')
+			{
+				
+			}
+		else
+			count += ft_putchar(format[i]);
+		}
+		i++;
+	}
+	va_end (list);
 	return (count);
 }
